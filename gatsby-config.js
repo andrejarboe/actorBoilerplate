9e69cef11d85bc06a8d5c4ac4998a7d62bb7c601
andrejarboe <andrejarboe@gmail.com>
2018-09-06 15:37:42 -0400

    add slug again

--- gatsby-config.js
+++ gatsby-config.js
@@ -13,5 +13,12 @@ module.exports = {
       },
     },
     'gatsby-transformer-remark',
+    {
+      resolve: 'gatsby-plugin-netlify-cms',
+      options: {
+        modulePath: `${__dirname}/src/cms/cms.js`,
+      },
+    },
+    'gatsby-plugin-netlify', // make sure to keep it last in the array
   ],
 }
