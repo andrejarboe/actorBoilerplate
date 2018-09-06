bddadf77b706b866cfb42d48c219e57127961672
andrejarboe <andrejarboe@gmail.com>
2018-09-06 16:54:03 -0400

    deleted the crap

--- gatsby-config.js
+++ gatsby-config.js
@@ -13,12 +13,5 @@ module.exports = {
       },
     },
     'gatsby-transformer-remark',
-    {
-      resolve: 'gatsby-plugin-netlify-cms',
-      options: {
-        modulePath: `${__dirname}/src/cms/cms.js`,
-      },
-    },
-    'gatsby-plugin-netlify', // make sure to keep it last in the array
   ],
 }
